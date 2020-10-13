9bbcde65ac3e46717d6288d8b7cab1ba4941abfc
Abuu Kantu <50331116+AbuuKantu@users.noreply.github.com>
2020-10-13 22:42:04 +0300

    Add files via upload
    User interface Design For A passenger

---
+++ js/book.js
@@ -0,0 +1,11 @@
+function book(){
+    
+    var info1 = document.getElementById('text1').value;
+    var  info2 = document.getElementById('text2').value;
+    var   info3 = document.getElementById('text3').value;
+    var   info4= document.getElementById('text4').value;
+    var   info5 = document.getElementById('text5').value;
+    var  info6  = document.getElementById('text6').value;
+    var  info7 = document.getElementById('text7').value;
+    var   info8  = document.getElementById('text8').value
+}
